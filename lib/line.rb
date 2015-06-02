5e3c710ae30e36ec1674fd4ccf65207a9bbf1278
Rajesh.Sah <rajesh.sah@urbanladder.com>
2015-06-02 15:11:57 +0530

    Checking if lines are equal

--- lib/line.rb
+++ lib/line.rb
@@ -4,9 +4,21 @@ class Line
         @y1_co_ordinate=y1
         @x2_co_ordinate=x2
         @y2_co_ordinate=y2
-
 	end
+
 	def length
 		length = Math.sqrt((@x2_co_ordinate-@x1_co_ordinate) ** 2 + (@y2_co_ordinate-@y1_co_ordinate) ** 2)
 	end
+    
+    def == line2
+    	if ((@x1_co_ordinate == line2.x1_co_ordinate) && (@x2_co_ordinate == line2.x2_co_ordinate) && (@y1_co_ordinate == line2.y1_co_ordinate) && (@y2_co_ordinate == line2.y2_co_ordinate)) || 
+    	   ((@x1_co_ordinate == line2.x2_co_ordinate) && (@x2_co_ordinate == line2.x1_co_ordinate) && (@y1_co_ordinate == line2.y2_co_ordinate) && (@y2_co_ordinate == line2.y1_co_ordinate))
+          return true
+        else
+          return false
+       	end
+    end
+    attr_reader :x1_co_ordinate, :y1_co_ordinate, :x2_co_ordinate, :y2_co_ordinate
+    
+
 end
--- spec/line_spec.rb
+++ spec/line_spec.rb
@@ -4,4 +4,19 @@ describe Line do
 		line = Line.new(1,2,4,6)
 		expect(line.length).to eq(5)
 	end
+    it "Test for compare two lines for equality based on the co-ordinates line1 (1,2) ,(3,4) : line2 (1,2) ,(3,4) " do
+    	line1 = Line.new(1,2,3,4)
+    	line2 = Line.new(1,2,3,4)
+    	expect(line1 == line2).to eq(true)
+    end	
+    it "Test for compare two lines for equality based on the co-ordinates line1 (1,2) ,(3,4) : line2 (2,3) ,(3,4) " do
+    	line1 = Line.new(1,2,3,4)
+    	line2 = Line.new(2,3,3,4)
+    	expect(line1 == line2).to eq(false)
+    end
+    it "Test for compare two lines for equality based on the co-ordinates line1 (1,2) ,(3,4) : line2 (3,4) ,(1,2) " do
+    	line1 = Line.new(1,2,3,4)
+    	line2 = Line.new(3,4,1,2)
+    	expect(line1 == line2).to eq(true)
+    end
 end
