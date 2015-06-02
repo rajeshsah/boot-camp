fa464bd4451e6587dd2e5cc39a44b909b4abff8a
Rajesh.Sah <rajesh.sah@urbanladder.com>
2015-06-02 18:14:41 +0530

    Area and perimeter of a rectangle using diagonal points

--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -1,11 +1,15 @@
 require 'spec_helper'
-
 describe Rectangle do
-	it "Check if  it returns correct perimeter length = 10 , breadth = 8 " do
+  it "Check if  it returns correct perimeter with digonal points (0,0) ,(10,8)" do
     expect(Rectangle.new(Point.new(0,0),Point.new(10,8)).perimeter).to eq(36) 
-	end	
-	
-  
-
-
+  end	
+  it "Check if  it returns correct perimeter with digonal points (0,0) ,(12,12)" do
+    expect(Rectangle.new(Point.new(0,0),Point.new(12,12)).perimeter).to eq(48) 
+  end
+  it "Check if  it returns correct area with digonal points (0,0) ,(10,8)" do
+    expect(Rectangle.new(Point.new(0,0),Point.new(10,8)).area).to eq(80) 
+  end
+  it "Check if  it returns correct area with digonal points (0,0) ,(12,12)" do
+    expect(Rectangle.new(Point.new(0,0),Point.new(12,12)).area).to eq(144) 
+  end
 end
